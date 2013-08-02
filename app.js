fce6f8cfe2f38ddf875f806068bb47968f1b6417
Alex Sax <alexanderesax@gmail.com>
2013-08-02 00:46:59 -0700

    Server now runs

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function(req, res){
-	res.render("test");
+	res.render("index");
 });
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
