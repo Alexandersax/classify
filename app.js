4d9687eb4404258f912c9775eb87464bf0ce0941
Alex Sax <alexanderesax@gmail.com>
2013-08-02 00:19:15 -0700

    server running

---
+++ app.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+	async = require('async'),
+	express = require('express'),
+	http = require('http');
+
+var app = express().use(express.static(__dirname));
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res){
+	res.render("test");
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
